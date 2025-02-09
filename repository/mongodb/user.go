5f253a5124bba8cf0e232ec7445f52a08f335c3d
Dito-7 <112498814+Dito-7@users.noreply.github.com>
2025-02-10 03:40:16 +0700

    feat : user logic

--- repository/mongodb/user.go
+++ repository/mongodb/user.go
@@ -2,7 +2,12 @@ package mongodb
 
 import (
 	"RestApi-Golang/model"
+	"context"
+	"fmt"
+	"log/slog"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -11,25 +16,99 @@ type MongoClient struct {
 }
 
 func (c MongoClient) CreateUser(user model.User) (string, error) {
-	return "", nil
+	result, err := c.Client.InsertOne(context.Background(), user)
+	if err != nil {
+		return "", err
+	}
+
+	return result.InsertedID.(primitive.ObjectID).Hex(), nil
 }
 
 func (c MongoClient) GetUserByID(id string) (model.User, error) {
-	return model.User{}, nil
+	docId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return model.User{}, fmt.Errorf("invalid ID")
+	}
+
+	var user model.User
+	filter := bson.D{{Key: "_id", Value: docId}}
+
+	err = c.Client.FindOne(context.Background(), filter).Decode(&user)
+	if err == mongo.ErrNoDocuments {
+		return model.User{}, fmt.Errorf("record not found")
+	} else if err != nil {
+		return model.User{}, err
+	}
+
+	return user, nil
 }
 
 func (c MongoClient) GetAllUsers() ([]model.User, error) {
-	return []model.User{}, nil
+	filter := bson.D{}
+	cur, err := c.Client.Find(context.Background(), filter)
+
+	if err != nil {
+		return []model.User{}, err
+	}
+	defer cur.Close(context.Background())
+
+	var users []model.User
+
+	for cur.Next(context.Background()) {
+		var user model.User
+		err := cur.Decode(&user)
+
+		if err != nil {
+			slog.Error("error decoding user", slog.String("error", err.Error()))
+			continue
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
 }
 
 func (c MongoClient) UpdateUserAgeByID(id string, age int) (int, error) {
-	return 0, nil
+	docId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return 0, fmt.Errorf("invalid ID")
+	}
+
+	filter := bson.D{{Key: "_id", Value: docId}}
+	updateStmnt := bson.D{{Key: "$set", Value: bson.D{{Key: "age", Value: age}}}}
+
+	result, err := c.Client.UpdateOne(context.Background(), filter, updateStmnt)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(result.ModifiedCount), nil
 }
 
 func (c MongoClient) DeleteUserByID(id string) (int, error) {
-	return 0, nil
+	docId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return 0, fmt.Errorf("invalid ID")
+	}
+
+	filter := bson.D{{Key: "_id", Value: docId}}
+
+	result, err := c.Client.DeleteOne(context.Background(), filter)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(result.DeletedCount), nil
 }
 
 func (c MongoClient) DeleteAllUsers() (int, error) {
-	return 0, nil
+	filter := bson.D{}
+
+	result, err := c.Client.DeleteMany(context.Background(), filter)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(result.DeletedCount), nil
 }
